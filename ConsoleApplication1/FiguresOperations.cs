2c7dde0d1bf32749f8da9f72c4a39a5dc4d99d94
Koperfild <avalancher2@rambler.ru>
2016-03-31 04:55:02 +0300

    Done

---
+++ ConsoleApplication1/FiguresOperations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class FiguresOperations
+    {
+        /// <summary>
+        /// Calculates square of right triangle
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static double Square(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+                throw new ArgumentException("Triangle side cannot be less or equal zero");
+
+            double[] sides = new double[] { a, b, c };
+            Array.Sort< double>(sides);
+            Array.Reverse(sides);//sides[0] будет гипотенузой как самая большая
+            //Если теорема Пифагора не выполняется выкидываем exception
+            if (Math.Pow(sides[0],2) != (Math.Pow(sides[1],2) + Math.Pow(sides[2],2)))
+            {
+                throw new ArgumentException("Triangle with such sides doesn't exist");
+            }
+
+            return sides[1] * sides[2] / 2;//Площадь прямоугольного треугольника
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void IncorrectTriangle1()
+        {
+            FiguresOperations.Square(11, 4, 5);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void IncorrectTriangle2()
+        {
+            FiguresOperations.Square(4, 11, 5);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void IncorrectTriangle3()
+        {
+            FiguresOperations.Square(5, 4, 11);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NegativeSizes1()
+        {
+            FiguresOperations.Square(-3, 4, 5);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NegativeSizes2()
+        {
+            FiguresOperations.Square(3,-4,5);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NegativeSizes3()
+        {
+            FiguresOperations.Square(3, 4, -5);
+        }
+        [TestMethod]
+        public void CorrectCalculation()
+        {
+            Assert.AreEqual(6, FiguresOperations.Square(3, 4, 5));
+        }
+        [TestMethod]
+        public void CorrectCalculation1()
+        {
+            Assert.AreEqual(6, FiguresOperations.Square(4, 5, 3));
+        }
+        [TestMethod]
+        public void CorrectCalculation2()
+        {
+            Assert.AreEqual(60, FiguresOperations.Square(8, 15, 17));
+        }
+    }
+}
